67d1f097d19baef7628b1951ed422d928102a4c5
Jartto <315764765@qq.com>
2022-11-01 09:09:28 +0800

    feat: update args

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+}
+
+
+
+// cargo run 命令：["target/debug/minigrep"]
+// cargo run hell jartto 命令：["target/debug/minigrep", "hell", "jartto"]
