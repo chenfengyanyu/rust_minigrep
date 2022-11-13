2dfc6f48a6aba786edc519e2cea3c9667a703a1b
Jartto <315764765@qq.com>
2022-11-13 20:23:40 +0800

    feat: udpate struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    // let query = &args[1];
+    // let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("文件读取异常");
+    println!("text: \n{}", contents);
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
 
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
+}
 
 // cargo run 命令：["target/debug/minigrep"]
-// cargo run hell jartto 命令：["target/debug/minigrep", "hell", "jartto"]
+// cargo run hello jartto 命令：["target/debug/minigrep", "hello", "jartto"]
